c32270aac86fda86009218bee83914f222616f51
João Pedro Mayrink <pepemayrink@gmail.com>
2025-07-17 16:27:50 -0300

    ✨ feat(global): add animations file

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'p-header',
@@ -7,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './header.scss'
 })
 export class Header {
+
+  router = inject(Router);
+
+  navigateToHome() {
+    this.router.navigate(['/']);
+  }
 
 }
