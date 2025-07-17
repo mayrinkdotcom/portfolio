4dc9d6395133c61e5e9b9eae35ea46d6a54cafd7
João Pedro Mayrink <joaopmayrinkj@gmail.com>
2025-07-17 11:25:30 -0300

    ✨ feat(global): add layout component

---
+++ src/app/components/layout/layout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Header } from '../header/header';
+
+@Component({
+  selector: 'p-layout',
+  imports: [Header],
+  templateUrl: './layout.html',
+  styleUrl: './layout.scss'
+})
+export class Layout {
+
+}
